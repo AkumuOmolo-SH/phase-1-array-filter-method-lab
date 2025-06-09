063674dedb517576bbb0a22d2a5d5a1fb23a113f
AkumuOmolo <akumu.omoloh@gmail.com>
2025-06-09 22:54:03 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, nameToMatch) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === nameToMatch.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, queryString) {
+  return drivers.filter(function(driver) {
+    return driver.startsWith(queryString);
+  });
+}
+
+function matchName(drivers, matchString) {
+  return drivers.filter(function(driver) {
+    return driver.name === matchString;
+  });
+}
